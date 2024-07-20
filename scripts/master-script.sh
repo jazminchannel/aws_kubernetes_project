5406545f79ac422ef3c61b87926a56ac931b34fa
Jazmin Channel <jazminchannel@Jazmins-Air.attlocal.net>
2024-07-20 18:43:40 -0500

    ansible updated

--- scripts/master-script.sh
+++ scripts/master-script.sh
@@ -11,9 +11,9 @@ ff00::0 ip6-mcastprefix
 ff02::1 ip6-allnodes
 ff02::2 ip6-allrouters
 ff02::3 ip6-allhosts
-10.0.24.108 k8s-control
-10.0.24.4 k8s-worker1
-10.0.24.15 k8s-worker2
+10.0.24.73 k8s-control
+10.0.24.155 k8s-worker1
+10.0.24.214 k8s-worker2
 EOF
 cat << EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
@@ -35,14 +35,18 @@ sudo containerd config default | sudo tee /etc/containerd/config.toml
 sudo systemctl restart containerd
 sudo swapoff -a
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
+KUBE_LATEST=$(curl -L -s https://dl.k8s.io/release/stable.txt | awk 'BEGIN { FS="." } { printf "%s.%s", $1, $2 }')
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/${KUBE_LATEST}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${KUBE_LATEST}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubelet=1.27.0-00 kubeadm=1.27.0-00 kubectl=1.27.0-00
+# sudo apt-get install -y kubelet=1.27.0-00 kubeadm=1.27.0-00 kubectl=1.27.0-00
+sudo apt-get install -y --allow-change-held-packages kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
-sudo kubeadm init --pod-network-cidr 192.168.0.0/16 --kubernetes-version 1.27.0
+sudo crictl config \
+        --set runtime-endpoint=unix:///run/containerd/containerd.sock \
+        --set image-endpoint=unix:///run/containerd/containerd.sock
+sudo kubeadm init --pod-network-cidr 192.168.0.0/16 --kubernetes-version 1.30.3
 sudo mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
